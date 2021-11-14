a17765c8ce86cbe2430286bff7012701987e9f97
Christinamelwani <90821809+Christinamelwani@users.noreply.github.com>
2021-11-15 00:10:43 +0700

    Create selection-sort-with-doubles.c

---
+++ selection-sort-with-doubles.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double GetUserInput (void);
+double * GetArray(int ArraySize);
+void SelectionSort (double arr[], int limit);
+void swap (double *i, double *j);
+void Print(double *arr, int size);
+
+int main(void)
+{
+    //Take input from user in the form of an array of doubles:
+    printf("How many numbers would you like to sort? ");
+    int NumberOfDoubles = GetUserInput();
+    double *arr = GetArray(NumberOfDoubles);
+
+    //Print the unsorted array:
+    printf("Unsorted Array: \n");
+    Print(arr, NumberOfDoubles);
+
+    //Sort the array:
+    SelectionSort(arr, NumberOfDoubles);
+    //Print the sorted array:
+    printf("\nSorted Array:\n");
+    Print(arr, NumberOfDoubles);
+    return 0;
+}
+
+double GetUserInput (void)
+{
+    //Take input from user in the form of a number:
+    double Input = 0;
+    while (Input == 0)
+    {
+        char buffer[20];
+        fgets(buffer, 20, stdin);
+        Input = atof(buffer);
+        if (Input == 0)
+        {
+            printf("Invalid input! Try again:\n");
+        }
+    }
+    return Input;
+}
+
+double * GetArray(int ArraySize)
+{
+    double *arr = malloc(sizeof(double) * ArraySize);
+    for (int i = 0; i < ArraySize; i++)
+    {
+        printf("Enter the %d%s number: ", i + 1,
+               (i + 1) % 10 == 1? "st":
+               (i + 1) % 10 == 2? "nd":
+               (i + 1) % 10 == 3? "rd":
+                "th");
+        arr[i] = GetUserInput();
+    }
+    return arr;
+}
+
+void SelectionSort (double arr[], int limit)
+{
+    for (int i = 0; i < limit; i++)
+    {
+        int small = i;
+        for (int j = i + 1; j < limit; j++)
+        {
+             if (arr[j] < arr[small])
+             {
+                 small = j;
+             }
+        }
+        swap(&arr[i], &arr[small]);
+    }
+}
+
+void swap (double *i, double *j)
+{
+    double temp = *i;
+    *i = *j;
+    *j = temp;
+}
+
+
+void Print(double *arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("%.2lf ", arr[i]);
+    }
+}
